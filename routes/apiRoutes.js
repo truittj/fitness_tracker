3ccc24736aac062ccd8a92357c3abf6b7df30c33
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-06-25 20:10:03 -0500

    Got the APIs structured... IDK why they are working. Will be seeing my tutor in the AM

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 const db = require("../models");
 
-module.exports = function(app){	
+module.exports = (app) => {	
     
     app.get("/api/workouts", (req,res) => {
 	db.Workout.find({})
@@ -11,4 +11,20 @@ module.exports = function(app){
             res.json(err)
         })
     });
+
+    app.put("/api/workouts/:id", (req, res) => {
+    let id = req.params.id;
+
+    db.Workout.update (
+        { _id: id },
+        { $push: { exercises: req.body }}
+    )
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+    });
+
 }
