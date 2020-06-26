1ca9aa78ff70fb7eb7f77e513d055923705dcb30
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-06-26 16:17:43 -0500

    Fixing Mongo Deployment

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
